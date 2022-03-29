08c9513968eba27c3397a117d78c1f36f45d0ada
苑坤龙 <yuankl@publink.cn>
2022-03-29 19:52:35 +0800

    chore:基础

--- router-test/src/components/HelloWorld.vue
+++ router-test/src/components/HelloWorld.vue
@@ -1,32 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1 @click="qwe">{{ msg }}</h1>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -34,7 +9,34 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      default: '哈哈哈哈哈哈哈'
+    },
+  },
+  methods: {
+    qwe(){
+      const queryName = 'queryName';
+
+      this.$router.push('/toc');
+      this.$router.push({ path: '/tod' });
+      this.$router.push({ name: 'test', params: { qwe: '123' } });
+      // 如果提供了path， params会被忽略 可以使用下面方式
+      this.$router.push({ path: '/to', query: { qwe: 123 } });
+      this.$router.push({ name: 'test', parmas: { qwe: 123 } });
+      this.$router.push({ path: `/to/${queryName}` });
+      this.$router.push(`/to/${queryName}`);
+
+      // replace是替换当前路由 不会有记录
+      this.$router.replace('/to');
+
+      // $router.push() === window.history.pushState()
+      // $router.replace() === window.history.replaceState()
+      // $router.go() === window.history.go()
+    }
+  },
+  beforeRouteUpdate(to, form){
+    console.log(to, form);
   }
 }
 </script>
--- router-test/src/router/index.js
+++ router-test/src/router/index.js
@@ -2,10 +2,32 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 
 import HelloWorld from '@src/components/HelloWorld';
+import HelloWorld1 from '@src/components/HelloWorld1';
+// const User = {
+//     template: '<div>User</div>'
+// }
 
 const routes = [
-    { path: '/', component: HelloWorld },
-    
+    { path: '/', redirect: '/hello' },
+    { path: '/test1/:id/test2/:qwe', name: 'test' , component: HelloWorld },
+
+    {
+        path: '/hello',
+        component: HelloWorld,
+        children: [
+            {
+                path: '/one', 
+                component: HelloWorld1
+            }
+        ]
+    },
+    {
+        path: '/',
+        redirect: (to,from)=>{
+            console.log(to,from);
+            return { path: '/search', query: { qwe: 123 } }
+        }
+    },
 ]
 
 Vue.use(VueRouter);
